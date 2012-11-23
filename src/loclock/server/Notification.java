caa23300487b9c92cee69a71dffd6ace8df2bd6b
Tian Cai <caitian1989@gmail.com>
2012-11-23 14:44:50 -0800

    Second set of Javadocs up

--- src/loclock/server/Notification.java
+++ src/loclock/server/Notification.java
@@ -42,6 +42,7 @@ public class Notification {
 	private String type;
 	
 	/**
+	 * Class constructor of Notification
 	 * 
 	 */
 	public Notification() {
@@ -51,9 +52,11 @@ public class Notification {
 	}
 	
 	/**
-	 * @param fromUser
-	 * @param toUser
-	 * @param content
+	 * Class constructor of Notification
+	 * 
+	 * @param fromUser the name of of the user who owns the notification
+	 * @param toUser the name of the user who reads the notification
+	 * @param content the content of the notification
 	 */
 	public Notification(String fromUser, String toUser, String content, String eventName, String type) {
 		this();
@@ -65,62 +68,80 @@ public class Notification {
 	}
 
 	/**
-	 * @return
+	 * Get the identifier of the request
+	 * 
+	 * @return id the id of the notification
 	 */
 	public Long getId() {
 		return id;
 	}
 
 	/**
-	 * @return
+	 * Get the username of the owner of the notification content.
+	 * 
+	 * @return fromUser the name of the owner of the notification content
 	 */
 	public String getFromUser() {
 		return fromUser;
 	}
 
 	/**
-	 * @param fromUser
+	 * Set the owner of the notification content.
+	 * 
+	 * @param fromUser the name of the notification content owner
 	 */
 	public void setFromUser(String fromUser) {
 		this.fromUser = fromUser;
 	}
 
 	/**
-	 * @return
+	 * Get the username of the notification reader.
+	 * 
+	 * @return the name of the notification reader
 	 */
 	public String getToUser() {
 		return toUser;
 	}
 
 	/**
-	 * @param toUser
+	 * Set the reader of the notification content.
+	 * 
+	 * @param toUser the name of the reader of the notification content
 	 */
 	public void setToUser(String toUser) {
 		this.toUser = toUser;
 	}
 
 	/**
-	 * @return
+	 * Get the content of the notification
+	 * 
+	 * @return the content of the notification
 	 */
 	public String getContent() {
 		return content;
 	}
 
 	/**
-	 * @param content
+	 * Set the content of the notification.
+	 * 
+	 * @param content the content of the notification
 	 */
 	public void setContent(String content) {
 		this.content = content;
 	}
 
 	/**
-	 * @return
+	 * Get the name of the event.
+	 * 
+	 * @return eventName the name of the event
 	 */
 	public String getEventName() {
 		return eventName;
 	}
 
 	/**
+	 * Set the name of the event.
+	 * 
 	 * @param eventName
 	 */
 	public void setEventName(String eventName) {
@@ -128,14 +149,18 @@ public class Notification {
 	}
 
 	/**
-	 * @return
+	 * Get the date of the notification creation.
+	 * 
+	 * @return the date of creation
 	 */
 	public Date getNotificationCreationDate() {
 		return notificationCreationDate;
 	}
 	
 	/**
-	 * @return
+	 * Get the date of the notification creation in date format.
+	 * 
+	 * @return the date of notification in the date format
 	 */
 	public String getNotificationCreationDateAsReadable(){
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -143,16 +168,28 @@ public class Notification {
 	}
 
 	/**
-	 * @param notificationCreationDate
+	 * Set the creation date of the notification.
+	 * 
+	 * @param notificationCreationDate the creation date of the notification
 	 */
 	public void setNotificationCreationDate(Date notificationCreationDate) {
 		this.notificationCreationDate = notificationCreationDate;
 	}
 
+	/**
+	 * Get the type of the notification.
+	 * 
+	 * @return type the type of the notification
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Set the type of the notification.
+	 * 
+	 * @param type
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
